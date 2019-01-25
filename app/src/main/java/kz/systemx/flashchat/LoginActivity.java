6bac3e8e3803fc0d2f538bacd5ad702c1b7cdbfc
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-01-25 19:06:51 +0900

    Login activity

--- app/src/main/java/kz/systemx/flashchat/LoginActivity.java
+++ app/src/main/java/kz/systemx/flashchat/LoginActivity.java
@@ -14,12 +14,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     // TODO: Add member variables here:
+    private FirebaseAuth mAuth;
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
@@ -44,12 +48,13 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         // TODO: Grab an instance of FirebaseAuth
-
+        mAuth = FirebaseAuth.getInstance();
     }
 
     // Executed when Sign in button pressed
     public void signInExistingUser(View v)   {
         // TODO: Call attemptLogin() here
+        attemptLogin();
 
     }
 
@@ -62,16 +67,44 @@ public class LoginActivity extends AppCompatActivity {
 
     // TODO: Complete the attemptLogin() method
     private void attemptLogin() {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
 
+        if (email.equals("") || password.equals("")) {
+            return;
+        } else {
+            Toast.makeText(this,"Login in progress ...", Toast.LENGTH_SHORT).show();
+        }
 
         // TODO: Use FirebaseAuth to sign in with email & password
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("FlashChat", "signInWithEmail() onComplete: " + task.isSuccessful());
+                if (!task.isSuccessful()){
+                    Log.d("FlashChat", "problem signing in" + task.getException());
+                    showErrorDialog("Problem signing in");
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainChatActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
 
+            }
+        });
 
 
     }
 
     // TODO: Show error on screen with an alert dialog
-
+    private void showErrorDialog(String message){
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 
 
 }
