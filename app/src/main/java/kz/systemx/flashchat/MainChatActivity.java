157cd79859d2c4f7e27966961c5a4d19ce1c2bdb
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-01-25 19:34:28 +0900

    Stored data

--- app/src/main/java/kz/systemx/flashchat/MainChatActivity.java
+++ app/src/main/java/kz/systemx/flashchat/MainChatActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -13,6 +14,8 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainChatActivity extends AppCompatActivity {
@@ -22,6 +25,7 @@ public class MainChatActivity extends AppCompatActivity {
     private ListView mChatListView;
     private EditText mInputText;
     private ImageButton mSendButton;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,7 @@ public class MainChatActivity extends AppCompatActivity {
 
         // TODO: Set up the display name and get the Firebase reference
         setupDisplayName();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
         // Link the Views in the layout to the Java code
         mInputText = (EditText) findViewById(R.id.messageInput);
@@ -37,10 +42,21 @@ public class MainChatActivity extends AppCompatActivity {
         mChatListView = (ListView) findViewById(R.id.chat_list_view);
 
         // TODO: Send the message when the "enter" button is pressed
-
+        mInputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                sendMessage();
+                return true;
+            }
+        });
 
         // TODO: Add an OnClickListener to the sendButton to send a message
-
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                sendMessage();
+            }
+        });
     }
 
     // TODO: Retrieve the display name from the Shared Preferences
@@ -56,8 +72,14 @@ public class MainChatActivity extends AppCompatActivity {
     }
 
     private void sendMessage() {
-
+        Log.d("FlashChat", "I sent message");
         // TODO: Grab the text the user typed in and push the message to Firebase
+        String input = mInputText.getText().toString();
+        if (!input.equals("")) {
+            InstantMessage chat = new InstantMessage(input, mDisplayName);
+            mDatabaseReference.child("messages").push().setValue(chat);
+            mInputText.setText("");
+        }
 
     }
 
